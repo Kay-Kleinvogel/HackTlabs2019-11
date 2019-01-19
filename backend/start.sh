c89a5c245a3ee8b6e5cfd98df1ad8471a86e1c69
Jan Christoph Ebersbach <jceb@e-jc.de>
2019-01-19 22:30:06 +0100

    Set up custom jolocom installation

---
+++ backend/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+set -e
+cd demo-sso
+# yarn --dev
+# yarn
+# yarn add --dev webpack-cli copy-webpack-plugin @types/react-dom @types/history @types/react-router @types/socket.io-client
+yarn start
