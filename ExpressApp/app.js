0ceb2820a1ba327f656471e542fab56a7cca551d
Ironlors <kay@Kays-MacBook-Pro.local>
2019-01-20 10:15:04 +0100

    imported mongodb

--- ExpressApp/app.js
+++ ExpressApp/app.js
@@ -1,39 +1,21 @@
-var contracts = [
-    {
-        GUID:"ce2b4753-dd64-4e30-8063-2194814d982f",
-        owner:"9c3fc7bd-3feb-4c9d-9770-e40d5991ba90",
-        recipient:"a1624e68-d048-4d05-9255-a85aa5293a67",
-        contract_name:"doctor",
-        frequency:"30",
-        start_date:"2018-01-01",
-        end_date:"2020-01-01"
-    },
-    {
-        GUID:"e7818820-a0c6-4d17-a103-739bbc05d399",
-        owner:"c35ee732-fa57-4d18-9707-9b0712c0cfde",
-        recipient:"9e2c6878-66c4-4118-a800-e34fda655626",
-        contract_name:"insurance",
-        frequency:"364",
-        start_date:"2018-01-01",
-        end_date:"2025-01-01"
-    },
-    {
-        GUID:"4c4dfe24-eee3-491f-86d0-1c74f67d7430",
-        owner:"f571ed6d-b9a6-4f3c-83d1-d0df89fe52ec",
-        recipient:"2e73f2c1-5c26-4ec4-a361-20bc500936fa",
-        contract_name:"trainer",
-        frequency:"7",
-        start_date:"2018-01-01",
-        end_date:"2018-02-01"
-    },
-]
-
+var contracts = [];
 
 var express = require('express');
 var app = express();
 
 // adds "public" directory to the called directories
 app.use(express.static("public"));
+
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/Demo_Transaction_Data.TlabsHack", function (err, db) {
+   
+     if(err) throw err;
+
+     //Write databse Insert/Update/Query code here..
+                
+});
 
 app.get("/menu",function(reg, res){
     
--- ExpressApp/public/js/splash.js
+++ ExpressApp/public/js/splash.js
@@ -4,3 +4,11 @@ body.on("click",function(){
 });
 
 // http://192.168.16.183:3000/menu"
+
+var progress = 20;
+function progressUp(value) {
+  progress = progress + value;
+  if(progress <= 100){
+     $(".fill").css("height", progress+"%");
+  }
+}
