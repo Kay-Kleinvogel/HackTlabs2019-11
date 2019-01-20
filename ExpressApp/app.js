25303f536731312bfb45b3a128156739a541bf8b
Ironlors <kay@Kays-MacBook-Pro.local>
2019-01-20 04:11:27 +0100

    EJS IS WORKING!!!!

--- ExpressApp/app.js
+++ ExpressApp/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.render("index.ejs")
 });
 
 app.listen(3000, function () {
