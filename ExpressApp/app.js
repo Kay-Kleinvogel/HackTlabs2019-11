eede20c2a4bbf1377b1f5a6b26d6d09e306802ee
Ironlors <kay@Kays-MacBook-Pro.local>
2019-01-20 05:01:26 +0100

    added routing & jse content for / and /menu

--- ExpressApp/app.js
+++ ExpressApp/app.js
@@ -2,8 +2,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.render("index.ejs")
+// adds "public" directory to the called directories
+app.use(express.static("public"));
+
+app.get("/menu",function(reg, res){
+    res.render("menu.ejs")
+    console.log("User visited /menu");
+});
+
+// leave this at the bottom since it's the catch all response
+app.get('/*', function (req, res) {
+    res.render("splash.ejs")
+    console.log("User visited /");
 });
 
 app.listen(3000, function () {
